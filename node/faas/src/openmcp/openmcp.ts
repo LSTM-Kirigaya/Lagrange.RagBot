cccd6eaa718d4fc5e8de9ad1d7b7b658b64fa2f1
kirigaya <Kirigaya>
2025-05-19 04:18:15 +0800

    add gh release tool

--- node/faas/src/openmcp/openmcp.ts
+++ node/faas/src/openmcp/openmcp.ts
@@ -5,6 +5,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 import { execSync, spawnSync } from 'child_process';
 import os from 'os';
+import { createTaskContext } from "../service/common";
 
 export const OPENMCP_CLIENT = os.homedir() + '/project/openmcp-client';
 
@@ -32,7 +33,7 @@ function getLastChangeLog() {
     const version = newContent.split('\n')[0];
     const updateContent = newContent.split('\n').slice(1).join('\n');
     const content = `✴️ openmcp client ${version} 更新内容\n\n` + updateContent.trim() + '\n\n在 vscode/trae/cursor 等编辑器的插件商城搜索【openmcp】就可以下载最新的版本了！';
-    return content
+    return content;
 }
 
 
@@ -54,9 +55,60 @@ app.post('/publish-openmcp-client', async (req: Request, res: Response) => {
 });
 
 
+async function getChangeLogEnglish(updateContent: string) {
+    const { taskLoop } = await createTaskContext();
+
+    const storage = {
+        messages: [],
+        settings: {
+            temperature: 0.7,
+            enableTools: [],
+            systemPrompt: '',
+            contextLength: 20
+        }
+    };
+
+    const message = '请将下面的更新日志翻译成 GitHub release 风格的英文说明，请只返回翻译后的结果，不要出现任何多余的前缀：\n' + updateContent;
+
+    try {
+        await taskLoop.start(storage, message);
+        const lastMessage = storage.messages.at(-1);
+        return lastMessage.content;
+    } catch (error) {
+        return updateContent;   
+    }
+}
+
+
 app.post('/publish-vsix', async (req: Request, res: Response) => {
     try {
         const { vsix, tool }  = req.body;
+        const result = execSync(tool + ' publish -i ' + vsix, { cwd: OPENMCP_CLIENT });
+        res.send({
+            code: 200,
+            msg: result
+        });
+    } catch (error) {
+        console.log(error);
+        res.send(error.toString());
+    }
+});
+
+app.post('/publish-github-release', async (req: Request, res: Response) => {
+    try {
+        const { vsix }  = req.body;
+
+        // 从 $OPENMCP_CLIENT/changelog.md 中读取 version
+        const changelog = fs.readFileSync(path.join(OPENMCP_CLIENT, 'CHANGELOG.md'), { encoding: 'utf-8' });
+        const newContent = changelog.split('## [main]')[1];
+        const version = newContent.split('\n')[0];
+        const tag = 'v'+ version;
+        const title = 'openmcp client '+ tag;
+        const updateContent = newContent.split('\n').slice(1).join('\n');
+        const notes = await getChangeLogEnglish(updateContent);
+
+        const tool = 'gh release create ' + tag + ' ' + vsix + ' --title ' + title + ' --notes ' + notes;
+
         const result = execSync(tool + ' publish -i ' + vsix, { cwd: OPENMCP_CLIENT });
         res.send({
             code: 200,
